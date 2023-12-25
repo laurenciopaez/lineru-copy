f3f4d22535cbd8ae2d7b0bab4da5497a60e6300b
laurenciopaez <laurencio314gmail.com>
2023-12-25 11:47:32 -0300

    Server

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,14 +1,15 @@
 const apiController = {
   getData: (req, res) => {
     const NewsAPI = require("newsapi");
-    const newsapi = new NewsAPI("9c1fa828bb9249b9a528b74bea143aa6");
+    const newsapi = new NewsAPI(process.env.API_KEY);
+    const today = new Date().toISOString().split('T')[0];
+    const yesterday = new Date(Date.now() - 864e5).toISOString().split('T')[0];
+    
     newsapi.v2
       .everything({
-        q: "bitcoin",
-        sources: "bbc-news,the-verge",
-        domains: "bbc.co.uk, techcrunch.com",
-        from: "2017-12-01",
-        to: "2017-12-12",
+        q: "bank",
+        from: yesterday,
+        to: today,
         language: "en",
         sortBy: "relevancy",
         page: 2,
@@ -16,8 +17,12 @@ const apiController = {
       .then((response) => {
         console.log(response);
         
-        res.json({message: 'Data ok'})
-      });
+        res.json({message: 'Data ok', data: response})
+      })
+      .catch( (error) => {
+        console.error(error);
+        res.status(500).json({ message: 'Error obtaining data'})
+      })
   },
 };
 
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const apiRoutes = require('./routes/apiRoutes.js')
-
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
